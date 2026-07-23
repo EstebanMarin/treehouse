122635d2b6cff7ff9a1ed3cebbe5ebf0e9381812
EstebanMarin <estebanmarin002@gmail.com>
2026-07-23 12:28:21 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String
+    action: VisitorAction,
+    age: i8,
 }
 
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str) -> Self {
 	Self {
 	    name: name.to_lowercase(),
-	    greeting: greeting.to_lowercase()
+	    action: VisitorAction::Probation,
+	    age: 0
 	}
     }
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
 }
 
 fn whats_your_name() -> String {
@@ -42,8 +53,31 @@ fn basic_example() {
     }
 }
 
+fn using_struct() {
+    let jacobo= Visitor::new("jacobo");
+    let giselle = Visitor::new("giselle");
+    let julieta = Visitor::new("julieta");
+    println!("{:#?}", jacobo);
+    let visitor_list = [
+	jacobo,
+	giselle,
+	julieta,
+    ];
+    println!("Whats your name?");
+    let name: String = whats_your_name();
+    let know_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+
+    match know_visitor {
+	Some(visitor) =>
+	    println!("{}", visitor.name),
+	    None => println!("Shooo")
+    }
+}
+
 fn main() {
-    
-   basic_example();
+    basic_example();
+    using_struct();
 }
 
